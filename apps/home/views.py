675fa951a67b45e1a6c0c09273845ae47cdb8d13
pouyafarahani <105992449+pouyafarahani@users.noreply.github.com>
2023-05-30 21:50:30 +0330

    add button to home and create product_list

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.views.generic import ListView
 
 from apps.product.models.product import ProductModel
@@ -8,3 +7,5 @@ class HomeView(ListView):
     model = ProductModel
     context_object_name = 'products'
     template_name = 'home/home.html'
+    # paginate_by = 16
+    paginate_by = 1
