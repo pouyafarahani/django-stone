31592464080d7224a02c6f96ebb360ddb89d4f3f
pouyafarahani <105992449+pouyafarahani@users.noreply.github.com>
2023-06-11 14:03:16 +0330

    change sakhtar proje

--- apps/product/models/tombstone.py
+++ apps/product/models/tombstone.py
@@ -7,5 +7,8 @@ from django.urls import reverse
 class TombstoneModel(BaseModel):
     image = models.ImageField(upload_to='cover_tombstone/')
 
+    def __str__(self):
+        return self.name
+
     def get_absolute_url(self):
         return reverse('product:product_detail', args=[self.pk])
