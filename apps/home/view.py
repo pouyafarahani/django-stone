e1a46350bca78246ec657a3fc78759df6d0f638d
pouyafarahani <105992449+pouyafarahani@users.noreply.github.com>
2023-06-15 15:46:46 +0330

    fix from database 4 product from 4 grop render in home.html

--- apps/home/view.py
+++ apps/home/view.py
@@ -4,5 +4,9 @@ from apps.product.models import TombstoneModel
 
 
 def home_view(request):
-    tombstone = TombstoneModel.objects.all()
-    return render(request, 'home/home.html', {'products': tombstone})
+    tombstone = TombstoneModel.objects.filter(group='tombstone')[:4]
+    inscription = TombstoneModel.objects.filter(group='inscription')[:4]
+    sandblast = TombstoneModel.objects.filter(group='sandblast')[:4]
+    wholesale = TombstoneModel.objects.filter(group='wholesale')[:4]
+    return render(request, 'home/home.html',
+                  {'tombstone': tombstone, 'inscription': inscription, 'sandblast': sandblast, 'wholesale': wholesale})
