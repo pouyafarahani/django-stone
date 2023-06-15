2d3e70cb21f057d71c0980bc181ddb1f95322381
pouyafarahani <105992449+pouyafarahani@users.noreply.github.com>
2023-06-15 15:57:23 +0330

    fix try-except for home/view.py

--- apps/home/view.py
+++ apps/home/view.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+from django.http.response import HttpResponse
 
 from apps.product.models import TombstoneModel
 
 
+@cache_page(600)
 def home_view(request):
-    tombstone = TombstoneModel.objects.filter(group='tombstone')[:4]
-    inscription = TombstoneModel.objects.filter(group='inscription')[:4]
-    sandblast = TombstoneModel.objects.filter(group='sandblast')[:4]
-    wholesale = TombstoneModel.objects.filter(group='wholesale')[:4]
-    return render(request, 'home/home.html',
-                  {'tombstone': tombstone, 'inscription': inscription, 'sandblast': sandblast, 'wholesale': wholesale})
+    try:
+        tombstone = TombstoneModel.objects.filter(group='tombstone')[:4]
+        inscription = TombstoneModel.objects.filter(group='inscription')[:4]
+        sandblast = TombstoneModel.objects.filter(group='sandblast')[:4]
+        wholesale = TombstoneModel.objects.filter(group='wholesale')[:4]
+        return render(request, 'home/home.html',
+                      {'tombstone': tombstone, 'inscription': inscription, 'sandblast': sandblast,
+                       'wholesale': wholesale})
+
+    except ConnectionError:
+        HttpResponse('سایت در حال بروزرسانی میباشد. لطفا کمی بعد دوباره تلاش کنید')
