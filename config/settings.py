f44480d2ef386aa86561a39bf77aad5edcb0fc14
pouyafarahani <105992449+pouyafarahani@users.noreply.github.com>
2023-06-17 20:06:18 +0330

    fix production

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3mf*kr$h#f9!9xqj*o!^+ee^mty7^+u53!t97zth+$ncvggv=s'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -120,3 +120,9 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+# production
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
